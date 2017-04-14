c4979b6927e89087c3a86f159bc11146c821f455
Gillian Shields <shieldsg@umich.edu>
2017-04-14 13:51:24 -0400

    cache and databases

--- 206_data_access.py
+++ 206_data_access.py
@@ -16,15 +16,123 @@
 
 # Put all import statements you need here.
 import unittest
+import json
+import requests
+import tweepy
+import collections
+import sqlite3
+import twitter_info
 
 # Begin filling in instructions....
 
+#I want to test three recent Oscar nominee movies: La La Land, Moonlight, and Lion
+#For the users, I'll see who has the most favorites/retweets 
+#For the search, I'll see the data of amount of mentions of the movies during the actual Oscar night
+#Time constraint???
+
+#At least 1 function which gets and caches data from 1 of your data sources, and an invocation of each of those functions to show that they work 
+
+#TWITTER SET UP CODE
+consumer_key = twitter_info.consumer_key
+consumer_secret = twitter_info.consumer_secret
+access_token = twitter_info.access_token
+access_token_secret = twitter_info.access_token_secret
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+
+api = tweepy.API(auth, parser=tweepy.parsers.JSONParser())
+
+CACHE_FNAME = "finalproject_cache.json"
+try:
+	cache_file = open(CACHE_FNAME,'r')
+	cache_contents = cache_file.read()
+	cache_file.close()
+	CACHE_DICTION = json.loads(cache_contents)
+except:
+	CACHE_DICTION = {}
+
+#TWITTER API
+def getTwitterUsername(actor_username):
+	unique_identifier = "twitter_{}".format(username)
+	if unique_identifier in CACHE_DICTION:
+		print('using cached data for', username)
+		#twitter_results = CACHE_DICTION[unique_identifier]
+		statuses = CACHE_DICTION[unique_identifier]
+		pass
+	else:
+		print('getting data from internet for', username)
+		#twitter_results = api.user_timeline(id = username)
+		statuses = api.GetUserTimeline(screen_name=actor_username, count = 50) #how to get username from the actors listed in the dictionary???
+		CACHE_DICTION[unique_identifier] = twitter_results
+		f=open(CACHE_FNAME, 'w')
+		f.write(json.dumps(CACHE_DICTION))
+		f.close()
+
+	#return CACHE_DICTION[unique_identifier]
+	
+	return statuses
+
+def getTwitterMentions(title):
+	unique_identifier = "twitter_{}".format(username)
+	if unique_identifier in CACHE_DICTION:
+		print('using cached data for', username)
+		#twitter_results = CACHE_DICTION[unique_identifier]
+		statuses = CACHE_DICTION[unique_identifier]
+		pass
+	else:
+		print('getting data from internet for', username)
+		#twitter_results = api.user_timeline(id = username)
+		statuses = api.GetUserTimeline(screen_name=title, count = 50) #WHAT DO I DO FOR MENTIONS
+		CACHE_DICTION[unique_identifier] = twitter_results
+		f=open(CACHE_FNAME, 'w')
+		f.write(json.dumps(CACHE_DICTION))
+		f.close()
+
+	#return CACHE_DICTION[unique_identifier]
+	return statuses
+	
+
+getTwitterMentions('La La Land')
+
+#OMDB CACHING
+# r = request.get(url)
+# json.text = r.text
+# dict = json.loads(json.text)
+
+# dict['Title'] = 'La La Land'
+
+
+#Code that creates a database file and tables as your project plan explains, such that your program can be run over and over again without error and without duplicate rows in your tables.
+
+conn = sqlite3.connect('finalproject.db')
+cur = conn.cursor()
+
+#TWEETS TABLE
+cur.execute('DROP TABLE IF EXISTS Tweets')
+statement = 'CREATE TABLE IF NOT EXISTS '
+statement += 'Tweets (tweet TEXT, tweet ID INTEGER PRIMARY KEY, user TEXT NOT NULL, FOREIGN KEY (user) REFERENCES Users(user) on UPDATE SET NULL, movie TEXT NOT NULL, FOREIGN KEY (movie) REFERENCES Users(movie) on UPDATE SET NULL, number favorites INTEGER, number retweets INTEGER)'
+cur.execute(statement)
+
+#USERS TABLE
+cur.execute('DROP TABLE IF EXISTS Users')
+table = 'CREATE TABLE IF NOT EXISTS '
+table += 'Users (user_id INTEGER PRIMARY KEY, screen_name TEXT, num_favs INTEGER, retweets INTEGER)'
+cur.execute(table)
+
+#MOVIES TABLE
+cur.execute('DROP TABLE IF EXISTS Movies')
+table = 'CREATE TABLE IF NOT EXISTS '
+table += 'Movies (id INTEGER PRIMARY KEY, title TEXT, director TEXT, num_languages INTEGER, imdb_rating INTEGER, top_actor TEXT)'
+cur.execute(table)
+
+#At least enough code to load data into 1 of your database tables (this should accord with your instructions/tests)
 
 
 
 
 
 
+#Tests at the end of your file that accord with those instructions (will test that you completed those instructions correctly!)
 
 # Put your tests here, with any edits you now need from when you turned them in with your project plan.
 class Tests(unittest.TestCase):
