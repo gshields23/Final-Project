2e889fabeb7809a7d85d4df242f66a97735eb6d0
Gillian Shields <shieldsg@umich.edu>
2017-04-17 23:04:40 -0400

    1/2 done

--- 206_data_access.py
+++ 206_data_access.py
@@ -10,7 +10,7 @@
 # - At least 1 function which gets and caches data from 1 of your data sources, and an invocation of each of those functions to show that they work 
 # - Tests at the end of your file that accord with those instructions (will test that you completed those instructions correctly!)
 # - Code that creates a database file and tables as your project plan explains, such that your program can be run over and over again without error and without duplicate rows in your tables.
-# - At least enough code to load data into 1 of your dtabase tables (this should accord with your instructions/tests)
+# - At least enough code to load data into 1 of your database tables (this should accord with your instructions/tests)
 
 ######### END INSTRUCTIONS #########
 
@@ -22,6 +22,7 @@ import tweepy
 import collections
 import sqlite3
 import twitter_info
+
 
 # Begin filling in instructions....
 
@@ -42,7 +43,7 @@ auth.set_access_token(access_token, access_token_secret)
 
 api = tweepy.API(auth, parser=tweepy.parsers.JSONParser())
 
-CACHE_FNAME = "finalproject_cache.json"
+CACHE_FNAME = "206finalproject_cache.json"
 try:
 	cache_file = open(CACHE_FNAME,'r')
 	cache_contents = cache_file.read()
@@ -53,17 +54,16 @@ except:
 
 #TWITTER API
 def getTwitterUsername(actor_username):
-	unique_identifier = "twitter_{}".format(username)
+	unique_identifier = "twitter_{}".format(actor_username)
 	if unique_identifier in CACHE_DICTION:
-		print('using cached data for', username)
+		print('using cached data for', actor_username)
 		#twitter_results = CACHE_DICTION[unique_identifier]
 		statuses = CACHE_DICTION[unique_identifier]
 		pass
 	else:
-		print('getting data from internet for', username)
-		#twitter_results = api.user_timeline(id = username)
-		statuses = api.GetUserTimeline(screen_name=actor_username, count = 50) #how to get username from the actors listed in the dictionary???
-		CACHE_DICTION[unique_identifier] = twitter_results
+		print('getting data from internet for', actor_username)
+		statuses = api.user_timeline(screen_name=actor_username, count = 100) #how to get username from the actors listed in the dictionary???
+		CACHE_DICTION[unique_identifier] = statuses
 		f=open(CACHE_FNAME, 'w')
 		f.write(json.dumps(CACHE_DICTION))
 		f.close()
@@ -71,19 +71,20 @@ def getTwitterUsername(actor_username):
 	#return CACHE_DICTION[unique_identifier]
 	
 	return statuses
+
+actors = getTwitterUsername('La La Land')
 
 def getTwitterMentions(title):
-	unique_identifier = "twitter_{}".format(username)
+	unique_identifier = "twitter_{}".format(title)
 	if unique_identifier in CACHE_DICTION:
-		print('using cached data for', username)
+		print('using cached data for', title)
 		#twitter_results = CACHE_DICTION[unique_identifier]
 		statuses = CACHE_DICTION[unique_identifier]
 		pass
 	else:
-		print('getting data from internet for', username)
-		#twitter_results = api.user_timeline(id = username)
-		statuses = api.GetUserTimeline(screen_name=title, count = 50) #WHAT DO I DO FOR MENTIONS
-		CACHE_DICTION[unique_identifier] = twitter_results
+		print('getting data from internet for', title)
+		statuses = api.search(q=title, count = 100)
+		CACHE_DICTION[unique_identifier] = statuses
 		f=open(CACHE_FNAME, 'w')
 		f.write(json.dumps(CACHE_DICTION))
 		f.close()
@@ -92,82 +93,132 @@ def getTwitterMentions(title):
 	return statuses
 	
 
-getTwitterMentions('La La Land')
+mentions = getTwitterMentions('La La Land')
 
 #OMDB CACHING
-# r = request.get(url)
-# json.text = r.text
-# dict = json.loads(json.text)
+def getdata_omdb(title):
+	unique_identifier = "omdb_{}".format(title)
 
-# dict['Title'] = 'La La Land'
+	if unique_identifier in CACHE_DICTION:
+		print('using cached data for', title)
+		omdb_dict = CACHE_DICTION[unique_identifier]
+		return omdb_dict
 
+	else:
+		print('getting data from internet for', title)
+		params_dict = {}
+		params_dict['t'] = title
+		response = requests.get('http://www.omdbapi.com/', params = params_dict)
+		omdb_dict = json.loads(response.text)
+		CACHE_DICTION[unique_identifier] = omdb_dict
+		f=open(CACHE_FNAME, 'w')
+		f.write(json.dumps(CACHE_DICTION))
+		f.close()
+
+		return omdb_dict
+
+title = getdata_omdb('La La Land')
 
 #Code that creates a database file and tables as your project plan explains, such that your program can be run over and over again without error and without duplicate rows in your tables.
 
 conn = sqlite3.connect('finalproject.db')
 cur = conn.cursor()
 
-#TWEETS TABLE
-cur.execute('DROP TABLE IF EXISTS Tweets')
-statement = 'CREATE TABLE IF NOT EXISTS '
-statement += 'Tweets (tweet TEXT, tweet ID INTEGER PRIMARY KEY, user TEXT NOT NULL, FOREIGN KEY (user) REFERENCES Users(user) on UPDATE SET NULL, movie TEXT NOT NULL, FOREIGN KEY (movie) REFERENCES Users(movie) on UPDATE SET NULL, number favorites INTEGER, number retweets INTEGER)'
-cur.execute(statement)
 
 #USERS TABLE
 cur.execute('DROP TABLE IF EXISTS Users')
 table = 'CREATE TABLE IF NOT EXISTS '
-table += 'Users (user_id INTEGER PRIMARY KEY, screen_name TEXT, num_favs INTEGER, retweets INTEGER)'
+table += 'Users (user_id INTEGER PRIMARY KEY, user_screenname TEXT, num_favs INTEGER, retweets INTEGER)'
 cur.execute(table)
 
 #MOVIES TABLE
 cur.execute('DROP TABLE IF EXISTS Movies')
 table = 'CREATE TABLE IF NOT EXISTS '
-table += 'Movies (id INTEGER PRIMARY KEY, title TEXT, director TEXT, num_languages INTEGER, imdb_rating INTEGER, top_actor TEXT)'
+table += 'Movies (id INTEGER PRIMARY KEY, movie_title TEXT, director TEXT, num_languages INTEGER, imdb_rating INTEGER, top_actor TEXT)'
 cur.execute(table)
 
-#At least enough code to load data into 1 of your database tables (this should accord with your instructions/tests)
+#TWEETS TABLE
+cur.execute('DROP TABLE IF EXISTS Tweets')
+table = 'CREATE TABLE IF NOT EXISTS '
+table += 'Tweets (tweet TEXT, tweet_id INTEGER PRIMARY KEY, user_id TEXT NOT NULL, movie_title TEXT NOT NULL, number_favorites INTEGER, number_retweets INTEGER, FOREIGN KEY (user_id) REFERENCES Users(user_id) on UPDATE SET NULL, FOREIGN KEY (movie_title) REFERENCES Movies(movie_title) on UPDATE SET NULL)'
+cur.execute(table)
 
 
+conn.close()
 
+
+#TWITTER PROCESSING
+# users_userid = []
+# users_screenname = []
+# users_favs = []
+# users_descrip = []
+
+# for users in umich_tweets:
+# 	users_userid.append(users['user']['id_str'])
+# 	users_screenname.append(users['user']['screen_name'])
+# 	users_favs.append(users['user']['favourites_count'])
+# 	users_descrip.append(users['user']['description'])
+
+# user_list = list(zip(users_userid, users_screenname, users_favs, users_descrip))
+
+# for users in user_list:
+# 	z = 'INSERT OR IGNORE INTO Users VALUES (?, ?, ?, ?)'
+# 	cur.execute(z, users)
+
+# for x in umich_tweets:
+# 	for u in x['entities']['user_mentions']:
+# 		unique_identifier = "user_{}".format(u['screen_name'])
+# 		if unique_identifier in CACHE_DICTION:
+# 			my_var = CACHE_DICTION[unique_identifier]
+# 		else:
+# 			my_var = api.get_user(u['screen_name'])
+# 			CACHE_DICTION[unique_identifier] = my_var
+# 			f=open(CACHE_FNAME, 'w')
+# 			f.write(json.dumps(CACHE_DICTION))
+# 			f.close()
+
+
+#OMDB PROCESSING
+#for actors in omdb_info:
 
 
 
 #Tests at the end of your file that accord with those instructions (will test that you completed those instructions correctly!)
 
-# Put your tests here, with any edits you now need from when you turned them in with your project plan.
-class Tests(unittest.TestCase):
-	def test1(self): #tests that get_rating returns an integer
-		self.assertEqual(type(get_rating()), type(int))
+# # Put your tests here, with any edits you now need from when you turned them in with your project plan.
+# class Tests(unittest.TestCase):
+# 	def test1(self): #tests that get_rating returns an integer
+# 		self.assertEqual(type(get_rating()), type(int))
 
-	def test2(self): #tests that get_director returns a string
-		self.assertEqual(type(get_director()), type(""))
+# 	def test2(self): #tests that get_director returns a string
+# 		self.assertEqual(type(get_director()), type(""))
 
-	def test3(self): #tests that get_title returns a string
-		self.assertEqual(type(get_title()), type(""))
+# 	def test3(self): #tests that get_title returns a string
+# 		self.assertEqual(type(get_title()), type(""))
  
-	def test4(self): #tests that input of class Movie is a dictionary
-		self.assertEqual(type(Movie()), type({}))
+# 	def test4(self): #tests that input of class Movie is a dictionary
+# 		self.assertEqual(type(Movie()), type({}))
 
-	def test5(self): #tests that get_actors returns a list 
-		self.assertEqual(type(get_actors()), type([]))
+# 	def test5(self): #tests that get_actors returns a list 
+# 		self.assertEqual(type(get_actors()), type([]))
 
-	def test6(self): #tests that get_langs returns an integer
-		self.assertEqual(type(get_langs()), type(int))
+# 	def test6(self): #tests that get_langs returns an integer
+# 		self.assertEqual(type(get_langs()), type(int))
 
-	def test7(self): #tests that there are 6 rows in the Tweets table
-		conn = sqlite3.connect('final_project.db') #not sure what the db will be called yet
-		cur = conn.cursor()
-		cur.execute('SELECT * FROM Tweets');
-		result = cur.fetchall()
-		self.assertTrue(len(result[1])==6)
-		conn.close()
+# 	def test7(self): #tests that there are 6 rows in the Tweets table
+# 		conn = sqlite3.connect('final_project.db') #not sure what the db will be called yet
+# 		cur = conn.cursor()
+# 		cur.execute('SELECT * FROM Tweets');
+# 		result = cur.fetchall()
+# 		self.assertTrue(len(result[1])==6)
+# 		conn.close()
 
-	def test8(self): #tests that the list that get_actors contains strings
-		self.assertEqual(type(test8[0],type(["Brad Pitt", "George Clooney"])))
+# 	def test8(self): #tests that the list that get_actors contains strings
+# 		self.assertEqual(type(test8[0],type(["Emma Stone", "Ryan Gosling"])))
 
     
 
-if __name__ == "__main__":
-	unittest.main(verbosity=2)
+# if __name__ == "__main__":
+# 	unittest.main(verbosity=2)
 
 # Remember to invoke your tests so they will run! (Recommend using the verbosity=2 argument.)
